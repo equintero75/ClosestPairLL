8b75536dca359669a2e95e72d2d01ba4684d297d
equintero75 <112458262+equintero75@users.noreply.github.com>
2022-11-17 17:41:31 -0500

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package closest.pair.linked.list;
+
+/**
+ *
+ * @author esteb
+ */
+public class Nodo {
+
+    Nodo link;
+    int x;
+    int y;
+
+    public Nodo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
